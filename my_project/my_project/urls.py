c65fdd471522bcf2c0ab4083104d338b81182a36
libby <709898906@qq.com>
2020-08-26 19:48:09 +0800

    add files

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('attendence/',include('attendence.urls'))
+    # path('attendence/',include('attendence.urls'))
 ]
