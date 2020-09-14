a8ebdace669e5cd864325ea02e0ee7a945707e9f
libby <709898906@qq.com>
2020-09-14 20:31:36 +0800

    add files

--- my_project/attendence/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from attendence import views
-
-urlpatterns = [
-    path('view_show',views.view_show,name='records'),
-    path('add',views.add),
-    path('search',views.search,name='results'),
-]
